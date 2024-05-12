965e24f76f880ce357fbf1dee3b310744ef87037
triptishakya-dev <triptishakya.2001@gmail.com>
2024-05-12 23:01:12 +0530

    my bg changer

---
+++ script.js
@@ -0,0 +1,44 @@
+const btn1= document.getElementById('btn1')
+const btn2= document.getElementById('btn2')
+const btn3= document.getElementById('btn3')
+const btn4= document.getElementById('btn4')
+const btn5= document.getElementById('btn5')
+const btn6= document.getElementById('btn6')
+const btn7= document.getElementById('btn7') 
+const btn8= document.getElementById('btn8')
+const btn9= document.getElementById('btn9')
+const btn10= document.getElementById('btn10')
+
+btn1.addEventListener('click',function(){
+    document.body.style.backgroundColor ='red'
+})
+btn2.addEventListener('click',function(){
+    document.body.style.backgroundColor ='blue'
+})
+btn3.addEventListener('click',function(){
+    document.body.style.backgroundColor ='pink'
+})
+
+btn4.addEventListener('click',function(){
+    document.body.style.backgroundColor ='yellow'
+})
+btn5.addEventListener('click',function(){
+    document.body.style.backgroundColor ='purple'
+})
+btn6.addEventListener('click',function(){
+    document.body.style.backgroundColor ='green'
+})
+btn7.addEventListener('click',function(){
+    document.body.style.backgroundColor ='gray'
+})
+btn8.addEventListener('click',function(){
+    document.body.style.backgroundColor ='skyblue'
+
+})
+btn9.addEventListener('click',function(){
+    document.body.style.backgroundColor ='orange'
+
+})
+btn10.addEventListener('click',function(){
+    document.body.style.backgroundColor ='black'
+})
